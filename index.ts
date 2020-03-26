0016bd49c711e1ea46995ec820450f8c9dae3994
hideo54 <hideo54@hideo54.com>
2020-03-26 17:22:26 +0900

    画像ジェネレータ作成

--- index.ts
+++ index.ts
@@ -2,9 +2,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { makeDiffs } from './scraper';
+import visualizer from './visualizer';
 
 const job = async () => {
     const { lastUpdated, paragraphDiffs } = await makeDiffs(`${__dirname}/cache.json`);
+    const currentDate = new Date();
+    if (paragraphDiffs.filter(x => x.added || x.removed).length > 0) {
+        await visualizer(lastUpdated, currentDate, paragraphDiffs);
+    }
 };
-
 job();
---
+++ visualizer/index.ts
@@ -0,0 +1,47 @@
+import { ArrayChange } from 'diff';
+import pug from 'pug';
+import puppeteer from 'puppeteer';
+
+interface Diff {
+    type: 'added' | 'removed' | 'no-change-head' | 'no-change-tail';
+    value: string[];
+}
+
+export default async (lastUpdated: string, currentDate: Date, paragraphDiffs: ArrayChange<string>[]) => {
+    const diffs: Diff[] = []
+    const displayedNoChangeParagraphs = 4;
+    for (const [i, diff] of paragraphDiffs.entries()) {
+        if (diff.added) {
+            diffs.push({ type: 'added', value: diff.value});
+        } else if (diff.removed) {
+            diffs.push({ type: 'removed', value: diff.value});
+        } else {
+            if (i > 0) {
+                const value = diff.value.slice(0, displayedNoChangeParagraphs);
+                diffs.push({ type: 'no-change-head', value });
+            }
+            if (i < paragraphDiffs.length - 1) {
+                const value = diff.value.slice(-displayedNoChangeParagraphs);
+                diffs.push({ type: 'no-change-tail', value });
+            }
+        }
+    }
+    const currentDateStr = currentDate.toString();
+    const html = pug.renderFile(`${__dirname}/template.pug`, {
+        lastUpdated, currentDateStr, diffs,
+    });
+    const browser = await puppeteer.launch({
+        defaultViewport: {
+            width: 1200,
+            height: 800,
+            deviceScaleFactor: 2,
+        },
+        headless: process.env.NODE_ENV !== 'development',
+    });
+    const page = await browser.newPage();
+    await page.setContent(html);
+    await page.screenshot({
+        path: 'visualized.png',
+        fullPage: true,
+    });
+};
