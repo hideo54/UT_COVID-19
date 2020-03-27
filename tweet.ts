f915fae74f20d15e36cf8b14d05f1b3129fdf4ea
hideo54 <hideo54@hideo54.com>
2020-03-27 18:11:27 +0900

    大量の差分時画像が長すぎ投稿失敗する問題の一時的緊急対応

--- tweet.ts
+++ tweet.ts
@@ -4,10 +4,21 @@ import { promises as fs } from 'fs';
 const tweetBody = '講義オンライン化に関する情報サイト https://komabataskforce.wixsite.com/forstudents が更新されました。';
 
 export default async (client: Twitter) => {
-    const media = await fs.readFile(`${__dirname}/visualized.png`);
-    const res = await client.post('media/upload', { media });
-    await client.post('statuses/update', {
-        status: tweetBody,
-        media_ids: res.media_id_string,
-    });
+    const srcPath = `${__dirname}/visualized.png`;
+    const media = await fs.readFile(srcPath);
+    try {
+        const res = await client.post('media/upload', { media });
+        await client.post('statuses/update', {
+            status: tweetBody,
+            media_ids: res.media_id_string,
+        });
+    } catch (e) {
+        await client.post('statuses/update', {
+            status: tweetBody + '画像は間もなく添付いたします。',
+        });
+        const now = new Date();
+        const nowStr = now.toISOString();
+        const destPath = `${__dirname}/visualized-${nowStr}.png`;
+        await fs.copyFile(srcPath, destPath);
+    }
 };
