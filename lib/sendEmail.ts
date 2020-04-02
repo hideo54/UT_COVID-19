21a4c0d6f6d2ec834b6e8cb1526e59ba82c653e1
hideo54 <hideo54@hideo54.com>
2020-04-02 15:10:42 +0900

    Don't send an email when no diffs available #3

--- lib/sendEmail.ts
+++ lib/sendEmail.ts
@@ -19,51 +19,53 @@ const sendEmail = async () => {
     const yesterdayStr = yesterday.toISOString().slice(0, 10);
     const { paragraphDiffs } = await makeDiffs(`${__dirname}/../cache/${yesterdayStr}.json`, false);
     const diffs = generateDiff(paragraphDiffs, false);
-    const dataText = diffs.map(diff => {
-        let headText = '';
-        switch (diff.type) {
-            case 'added':
-                headText = '\n// 追加';
-                break;
-            case 'removed':
-                headText = '\n// 削除';
-                break;
-            case 'no-change-head':
-                headText = '\n// 変更なし';
-                break;
-            case 'no-change-tail':
-                headText = '// 中略';
-                break;
-        }
-        return `${headText}\n${diff.value.join('\n')}`;
-    });
-    const text = stripIndent`
-        @UT_COVID19 bot システムによる、自動配信メールです。
-        昨日 (${yesterday.format('M月D日')}) 22時から本日22時までの「講義オンライン化に関する情報サイト」の更新内容をお伝えいたします。
-        お問い合わせは本メールに対する返信でも承ることが可能です。
-        配信を停止したい場合にもお問い合わせください。
+    if (diffs.length > 0) {
+        const dataText = diffs.map(diff => {
+            let headText = '';
+            switch (diff.type) {
+                case 'added':
+                    headText = '\n// 追加';
+                    break;
+                case 'removed':
+                    headText = '\n// 削除';
+                    break;
+                case 'no-change-head':
+                    headText = '\n// 変更なし';
+                    break;
+                case 'no-change-tail':
+                    headText = '// 中略';
+                    break;
+            }
+            return `${headText}\n${diff.value.join('\n')}`;
+        });
+        const text = stripIndent`
+            @UT_COVID19 bot システムによる、自動配信メールです。
+            昨日 (${yesterday.format('M月D日')}) 22時から本日22時までの「講義オンライン化に関する情報サイト」の更新内容をお伝えいたします。
+            お問い合わせは本メールに対する返信でも承ることが可能です。
+            配信を停止したい場合にもお問い合わせください。
 
-        以下、更新内容になります。
-        ---` + `\n${dataText.join('\n')}\n// 後略`;
-    const fromAddress = process.env.EMAIL_ADDRESS_FROM!;
-    const toAddress = process.env.EMAIL_ADDRESS_TO!;
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-            type: 'OAuth2',
-            user: fromAddress,
-            serviceClient: key.client_id,
-            privateKey: key.private_key,
-        },
-    });
-    await transporter.verify();
-    await transporter.sendMail({
-        to: toAddress,
-        subject: '[自動] 講義情報サイト更新通知',
-        text,
-    });
+            以下、更新内容になります。
+            ---` + `\n${dataText.join('\n')}\n// 後略`;
+        const fromAddress = process.env.EMAIL_ADDRESS_FROM!;
+        const toAddress = process.env.EMAIL_ADDRESS_TO!;
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                type: 'OAuth2',
+                user: fromAddress,
+                serviceClient: key.client_id,
+                privateKey: key.private_key,
+            },
+        });
+        await transporter.verify();
+        await transporter.sendMail({
+            to: toAddress,
+            subject: '[自動] 講義情報サイト更新通知',
+            text,
+        });
+    }
 };
 
 export default sendEmail;
