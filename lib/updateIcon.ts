8c130f7e868700f2f8b72d7ba7d58b838fd82f2b
hideo54 <hideo54@hideo54.com>
2020-04-07 13:22:13 +0900

    Create a program to update icon #6

--- lib/updateIcon.ts
+++ lib/updateIcon.ts
@@ -1,12 +1,14 @@
 import sharp from 'sharp';
 import Twitter from 'twitter';
 
-export default async (client: Twitter, color: sharp.Color) => {
+export const updateIcon = async (client: Twitter, color: sharp.Color) => {
     const icon = await sharp({create: {
         width: 400, height: 400, channels: 3,
         background: color,
-    }}).toBuffer();
+    }}).png().toBuffer();
     await client.post('account/update_profile_image', {
         image: icon.toString('base64'),
     });
 };
+
+export default updateIcon;
